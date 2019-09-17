171ba456e8e8b6ee0d622286c2292db920d4f6af
Handan Zhang <hmix.cc@gmail.com>
2019-09-17 14:38:45 +0200

    added CRUD for task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.new(restaurant_strong_params).save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(restaurant_strong_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def restaurant_strong_params
+    params.require(:task).permit(:title, :details)
+  end
+end
